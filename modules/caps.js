36c29ceb1db7352cf3531b133d3410d3fe358da5
Saynka <sankakale@gmail.com>
2021-04-03 14:09:01 -0700

    thing

--- modules/caps.js
+++ modules/caps.js
@@ -1,6 +1,25 @@
-const Socket = require('socket.io');
+// 'use strict';
 
-// Event Pool or Hub
-const socket = new Socket();
+// require('../index.js')
 
-module.exports = socket;
+// io.on('connection', (socket) => {
+//   console.log('connected?', socket.id);
+// });
+// caps.on('connection', (socket) => {
+//   // step2
+//   socket.on('pickup', (payload) => {
+//     socket.broadcast.emit('pickup', payload);
+//     console.log('heard pickup')
+//   })
+
+//   socket.on('in-transit', (payload) => {
+//     socket.emit('in-transit', payload);
+//     console.log('heard in-transit')
+//   })
+//   socket.on('delivered', (payload) => {
+//     socket.broadcast.emit('delivered', payload);
+//     console.log('heard delivered')
+//   })
+//   console.log('connected to', socket.id)
+
+// });
